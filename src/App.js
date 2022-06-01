d1a44b30c37124165e5e04fbdc7d461645520148
samanthasang <samanthasang70@gmail.com>
2022-06-01 16:35:03 +0430

    add menu item

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homePage';
+import HomePage from './pages/homepage/homePage';
 
 
 function App() {
---
+++ src/pages/homepage/homePage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Directory from "../../components/directory/directory";
+
+
+import "./homepage.scss";
+
+
+const HomePage = () => (
+    <div className="homepage">
+        <Directory />
+    </div>
+)
+
+
+export default HomePage;
