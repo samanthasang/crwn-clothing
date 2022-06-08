925dd33ccad00c4a3562a50f70db4ba71648fdba
samanthasang <samanthasang70@gmail.com>
2022-06-08 16:18:56 +0430

    updated shop datea

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,11 @@
 import CartActionTypes from "./cart.types" 
+import { addItemToCart } from "./cart.utils";
+
+
+
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 }
 
 
@@ -11,7 +16,12 @@ const cartReducer = (state= INITIAL_STATE, action) => {
             return {
                 ...state,
                 hidden: !state.hidden
-            }
+            };
+        case CartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: addItemToCart(state.cartItems, action.payload)
+            };
     
         default:
             return state
